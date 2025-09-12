8304194db771ecb0daddc79f294e01d345fffa8d
AlexAnd012 <Alexandrandreev129@gmail.com>
2025-09-12 17:13:58 +0300

    1

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"TelegramBot/internal/config"
+	"fmt"
+	"log"
+	"net/http"
 
+	BotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	cfg := config.Load()
+
+	bot, err := BotApi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Authorized on account %s\n", bot.Self.UserName)
+
+	webhook, err := BotApi.NewWebhook(cfg.SelfURL + "/webhook")
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := bot.Request(webhook)
+	if err != nil || !resp.Ok {
+		log.Fatal(err)
+	}
+
+	updates := bot.ListenForWebhook("/webhook")
+
+	// позже заменить на порт от render
+
+	if err := http.ListenAndServe(":"+cfg.Port, nil); err != nil {
+		log.Fatal(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil {
+			continue
+		}
+		msg := BotApi.NewMessage(update.Message.Chat.ID, "hi")
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("send error: %v", err)
+		}
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	BotToken      string
+	SelfURL       string
+	DBUrl         string
+	TimeZone      string
+	WebHookSecret string
+	Port          string
+}
+
+func Load() Config {
+	cfg := Config{
+		BotToken:      os.Getenv("TELEGRAM_BOT_TOKEN"),
+		SelfURL:       os.Getenv("SELF_URL"),
+		DBUrl:         os.Getenv("DATABASE_URL"),
+		TimeZone:      os.Getenv("TIMEZONE"),
+		WebHookSecret: os.Getenv("TG_WEBHOOK_SECRET"),
+		Port:          os.Getenv("PORT"),
+	}
+
+	if cfg.BotToken == "" {
+		log.Fatal("BotToken is empty")
+	}
+	if cfg.DBUrl == "" {
+		log.Fatal("DataBase is not declared")
+	}
+	if cfg.SelfURL == "" {
+		log.Fatal("WebService is not declared")
+	}
+	if cfg.TimeZone == "" {
+		log.Fatal("TimeZone is empty")
+	}
+	if cfg.WebHookSecret == "" {
+		log.Fatal("WebHookSecret is empty")
+	}
+
+	return cfg
+}
